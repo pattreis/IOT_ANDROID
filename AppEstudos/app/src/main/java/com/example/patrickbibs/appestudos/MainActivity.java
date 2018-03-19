36e2f224ddd26a8c990f54a248a411661c2fd578
pattreis <patrick94reis@gmail.com>
2018-03-19 08:15:36 -0300

    app estudos

---
+++ AppEstudos/app/src/main/java/com/example/patrickbibs/appestudos/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.patrickbibs.appestudos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    String operador
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void calcular(View view) {
+
+    }
+
+    public void atualizaCalculo(View view) {
+
+    }
+
+    public void operador(View view) {
+
+    }
+}
